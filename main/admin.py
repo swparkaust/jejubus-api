ef3674420dd0cc475c57aba7ab07a52dd25f0a43
Sunwoo Park <swparkaust@me.com>
2020-05-26 17:21:09 +0900

    Save Station/Route relationships

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import (Route, Station, StationOtherName, Time)
+from .models import (Route, Station, StationOtherName, StationRoute, Time)
 
 admin.site.register(Station)
 admin.site.register(StationOtherName)
+admin.site.register(StationRoute)
 admin.site.register(Route)
 admin.site.register(Time)
